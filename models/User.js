f855814aed39b26414c60f999b0873cfb4007221
rai8 <hytonne@outlook.com>
2021-02-21 11:38:03 +0300

    hashing password

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 const { isEmail } = require('validator')
-
+const bcrypt = require('bcrypt')
 //define the schema
 const userSchema = new Schema({
   email: {
@@ -16,6 +16,14 @@ const userSchema = new Schema({
     required: [true, 'Please enter a password'],
     minlength: [6, 'Minimum password length is 6 characters'],
   },
+})
+
+//encrypt password before being saved to the database
+userSchema.pre('save', async function (next) {
+  //generate salt
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 //define model from the schema
